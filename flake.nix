1072a49f8c358d174055e5b33e01b06138bf4330
Chris Oboe <chris@oboe.email>
2022-02-22 20:43:34 +0100

    add build to flake

--- flake.nix
+++ flake.nix
@@ -8,7 +8,30 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in
-      {
+      rec {
+        packages = flake-utils.lib.flattenTree {
+          myWings-mirrorer = let lib = pkgs.lib; in
+            pkgs.buildGoModule {
+              pname = "myWings-mirrorer";
+              version = "0.0.1";
+
+              modSha256 = lib.fakeSha256;
+              vendorSha256 = "8RE26hK45rT/PWo4+pAmxm/MQt2n1N1Jh0JpPu2EbDE=";
+              src = ./src;
+
+              meta = {
+                description = "A tool to automatically download all the files of myWings your user has permissions to.";
+                homepage = "https://github.com/ChrisOboe/myWings-mirrorer";
+                license = lib.licenses.gpl3;
+                maintainers = [ "chris@oboe.email" ];
+                platforms = lib.platforms.linux ++ lib.platforms.darwin;
+              };
+            };
+        };
+
+        defaultPackage = packages.myWings-mirrorer;
+        defaultApp = packages.myWings-mirrorer;
+
         devShell = pkgs.mkShell {
           buildInputs = [
             # ide
